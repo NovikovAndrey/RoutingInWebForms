2a831c0874628ea24204595e784443d0e7643ab9
Андрей Новиков <novikov.andrey.v@hotmail.com>
2020-01-30 20:58:21 +0300

    Add dictionary+ end

--- RoutingInWebForms/Browsers.aspx.cs
+++ RoutingInWebForms/Browsers.aspx.cs
@@ -9,9 +9,23 @@ namespace RoutingInWebForms
 {
     public partial class Browsers : System.Web.UI.Page
     {
+        Dictionary<string, string> browsers = new Dictionary<string, string>();
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            browsers.Add("mozilla", "dsfsdfdsfdsfsdfsdfsd");
+            browsers.Add("chrome", "flsdkljsjklfdnjkdshfSKJDJFH");
+            browsers.Add("safary", "oiyrergjhduvndkjoiajflweknfw");
+            browsers.Add("opera", "uhrkebfjvhdfnreijdmvnalklmdnf");
+            browsers.Add("ie", "khdhgreugnljkfdhivbldfjkabngbgouibf");
+            try
+            {
+                string name = Page.RouteData.Values["name"] as string;
+                Label1.Text = browsers[name];
+            }
+            catch
+            {
+                Label1.Text = "Error";
+            }
         }
     }
 }
